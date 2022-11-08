d4b9ad2c1b0a650071fb22910dc8110da3b56789
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2022-11-07 19:30:55 -0500

    Add project files.

---
+++ CodeFirstDemo/Models/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstDemo.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name = MyContextDB") { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+
+
+
+    }
+}
